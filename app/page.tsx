07b22031cd33965fa751c1c9201a24a0405fb747
unknown <wallaceomatthew@gmail.com>
2026-01-06 15:42:05 -0600

    Dummy data plus tables row cells added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,109 @@
-export default function Home() {
+import Image from "next/image";
+import DataTable from "@/components/DataTable";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import {TrendingDown, TrendingUp} from "lucide-react";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+    {
+        header: "Name",
+        cellClassName: "name-cell",
+        cell: (coin)=> {
+            const item = coin.item;
+            return (
+                <Link href={`/coins/${item.id}`} className="flex items-center gap-2">
+                   <Image src={item.large} alt={item.name} width={36} height={36} />
+                    <p>{item.name}</p>
+                </Link>
+            );
+    }},
+    {
+        header: "24h Change",
+        cellClassName: "name-cell",
+        cell: (coin)=> {
+            const item = coin.item;
+            const isTrending = item.data.price_change_percentage_24h.usd > 0;
+            return(
+                <div className={cn('price-change', isTrending ? 'text-green-500': 'text-red-500' )}>
+                    <p className="flex items-center gap-1">
+                        {isTrending ? (
+                           <TrendingUp width={16} height={16} /> ) :
+                            <TrendingDown width={16} height={16} />
+                        }
+                        {item.data.price_change_percentage_24h.usd.toFixed(2)}%
+                    </p>
+                </div>
+            )
+        }
+    },
+    {
+        header: "Price",
+        cellClassName: "price-cell",
+        cell:(coin)=> {
+         return <p>${coin.item.data.price.toLocaleString()}</p>
+        },
+    }
+]
+
+const dummyTrendingCoins: TrendingCoin[] = [
+    {
+        item: {
+            id: "bitcoin",
+            name: "Bitcoin",
+            symbol: "BTC",
+            market_cap_rank: 1,
+            thumb: "/wired-outline-2588-logo-bitcoin-hover-roll.svg",
+            large: "/wired-outline-2588-logo-bitcoin-hover-roll.svg",
+            data: {
+                price: 89113.00,
+                price_change_percentage_24h: {
+                    usd: 2.5
+                }
+            }
+        }
+    },
+    {
+        item: {
+            id: "ethereum",
+            name: "Ethereum",
+            symbol: "ETH",
+            market_cap_rank: 2,
+            thumb: "/Atlantic-colored.svg",
+            large: "/Atlantic-colored.svg",
+            data: {
+                price: 2543.21,
+                price_change_percentage_24h: {
+                    usd: -1.2
+                }
+            }
+        }
+    }
+]
+
+export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+      <main className="main-container">
+        <section className={"home-grid"}>
+            <div id={"coin-overview"}>
+                <div className={"header"}>
+                    <Image src="/wired-outline-2588-logo-bitcoin-hover-roll.svg" alt="Bitcoinlogo" width={132} height={40}/>
+                    <div className={"info"}>
+                        <p>BitCoin / BTC</p>
+                        <h1>$89,113.00</h1>
+                    </div>
+                </div>
+            </div>
+            <p>Trending Coins</p>
+            <DataTable 
+                data={dummyTrendingCoins}
+                columns={columns}
+                rowKey={(coin) => coin.item.id}
+            />
+        </section>
+        <section className={"w-full mt-7 space-y-4"}>
+            <p>Categories</p>
+        </section>
       </main>
     </div>
   );
